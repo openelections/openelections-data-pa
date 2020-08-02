9a09064d7c44b4d4192da6cb5f08046f0c7ca36a
mjgaughan <58380965+mjgaughan@users.noreply.github.com>
2020-08-01 14:33:37 -1000

    Fixes to Bucks scraper

--- parsers/bucks_scraper.py
+++ parsers/bucks_scraper.py
@@ -3,34 +3,63 @@ from pdfreader import PDFDocument, SimplePDFViewer
 import csv
 
 precinct = " "
+partial = False
+office = " "
+party = " "
 
 def scraping_one_page(text_on_page, writer):
+    global office
+    global partial
     global precinct
+    global party
     #print(text_on_page)
     parties = ["DEM", "REP", "NPA"]
-    offices = ["PRESIDENT OF THE UNITED STATES", "ATTORNEY GENERAL", "AUDITOR GENERAL", "STATE TREASURER"]
+    offices = ["PRESIDENT OF THE UNITED STATES", "ATTORNEY GENERAL", "AUDITOR GENERAL", "STATE TREASURER", "REPRESENTATIVE IN CONGRESS", "REPRESENTATIVE IN THE GENERAL ASSEMBLY"]
     county = "Bucks"
     if text_on_page[13][:8] == "Precinct":
         precinct = text_on_page[13][9:]
     save = False
     name = False
     #print(precinct)
-    current_office = ' '
+    if partial:
+        i = 12
+        race = True
+        j = 1
+        while race:
+            candidate = text_on_page[i + j]
+            if candidate[:5] == "Page:":
+                partial = True
+                break
+            if " - " in candidate:
+                candidate = candidate[:candidate.index(" - ")]
+            votes = text_on_page[i + j + 1]
+            absentee = text_on_page[i + j + 3]
+            early = text_on_page[i + j + 4]
+            provisional = text_on_page[i + j + 5]
+            line = [county, precinct, office, " ", party, candidate, votes, absentee, early, provisional]
+            if candidate == "Total":
+                race = False
+                partial = False
+            #print(line)
+            #writer.writerow(line)
+            j += 6
     for i in range(len(text_on_page)):
         if " - " in text_on_page[i]:
-            office = text_on_page[i][:text_on_page[i].index(" - ")]
-            if "Democr" in text_on_page[i]:
-                party = "DEM"
-            elif "Republ" in text_on_page[i]:
-                party = "REP"
-            else:
-                party = "NPA"
-            if office in offices:
+            holding = text_on_page[i][:text_on_page[i].index(" - ")]
+            if holding in offices or holding[:-6] in offices or holding[:-5] in offices:
+                office = holding
                 j = 1
                 race = True
+                if "Democr" in text_on_page[i]:
+                    party = "DEM"
+                elif "Republ" in text_on_page[i]:
+                    party = "REP"
+                else:
+                    party = "NPA"
                 while race:
                     candidate = text_on_page[i + j]
                     if candidate[:5] == "Page:":
+                        partial = True
                         break
                     if " - " in candidate:
                         candidate = candidate[:candidate.index(" - ")]
@@ -55,7 +84,7 @@ if __name__ == "__main__":
         writer = csv.writer(csvfile)
         writer.writerow(["County", "Precinct", "Office", "District", "Party", "Candidate", "Votes", "Absentee", "Early", "Provisional"])
         for i in range(len((all_pages))):
-        #for i in range(3):
+        #for i in range(7):
             viewer.navigate(i + 1)
             viewer.render()
             text_on_page = viewer.canvas.strings
