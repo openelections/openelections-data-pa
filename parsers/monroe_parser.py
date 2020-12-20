05ff1d7e1536bcb9d14a8ead85cc25bbb1fbd364
Derek Willis <dwillis@gmail.com>
2020-12-20 14:52:50 -0500

    updated Monroe parser

--- parsers/monroe_parser.py
+++ parsers/monroe_parser.py
@@ -2,7 +2,7 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-election_id = '52'
+election_id = '53'
 county = 'Monroe'
 
 results = []
@@ -11,12 +11,15 @@ url = "http://agencies.monroecountypa.gov/elections/"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-offices = []
+offices = [
+    'Presidential Electors', 'Attorney General', 'Auditor General', 'State Treasurer', 'Representative in Congress 7th District', 'Representative in Congress 8th District', 'Representative in the General Assembly 115th District',
+    'Representative in the General Assembly 176th District', 'Representative in the General Assembly 189th District'
+]
 
-for el in soup.find_all('h4', attrs={'class':'panel-title'}):
-    if el.find('a'):
-        if 'Delegate' not in el.find('a').text.strip():
-            offices.append(el.find('a').text.strip())
+#for el in soup.find_all('h4', attrs={'class':'panel-title'}):
+#    if el.find('a'):
+#        if 'Delegate' not in el.find('a').text.strip():
+#            offices.append(el.find('a').text.strip())
 
 all_office_ids = [x['value'] for x in soup.find_all('button', attrs={'class':'btn btn-warning btnCandPrecincts'})]
 office_ids = []
@@ -25,22 +28,24 @@ for index, element in enumerate(all_office_ids):
     if index % 2 == 0:
         office_ids.append(element)
 
-office_ids = office_ids[0:len(offices)]
+office_ids = all_office_ids[0:len(offices)]
 
 offices_with_ids = zip(offices, office_ids)
 
 for office, office_id in offices_with_ids:
-    dem_url = f"http://agencies.monroecountypa.gov/elections/getData.ashx?partyname=DEMOCRATIC&electionid={election_id}&el_off_id={office_id}&type=precinct"
-    rep_url = f"http://agencies.monroecountypa.gov/elections/getData.ashx?partyname=REPUBLICAN&electionid={election_id}&el_off_id={office_id}&type=precinct"
-    r = requests.get(dem_url)
+    print(office)
+    url = f"http://agencies.monroecountypa.gov/elections/getData.ashx?partyname= &electionid={election_id}&el_off_id={office_id}&type=precinct"
+#    dem_url = f"http://agencies.monroecountypa.gov/elections/getData.ashx?partyname=DEMOCRATIC&electionid={election_id}&el_off_id={office_id}&type=precinct"
+#    rep_url = f"http://agencies.monroecountypa.gov/elections/getData.ashx?partyname=REPUBLICAN&electionid={election_id}&el_off_id={office_id}&type=precinct"
+#    r = requests.get(dem_url)
+#    for result in r.json():
+#        results.append([county, result['precinctName'], office, None, 'DEM', result['firstName']+ ' '+ result['lastName'], result['totalVotes']])
+    r = requests.get(url)
     for result in r.json():
-        results.append([county, result['precinctName'], office, None, 'DEM', result['firstName']+ ' '+ result['lastName'], result['totalVotes']])
-    r = requests.get(rep_url)
-    for result in r.json():
-        results.append([county, result['precinctName'], office, None, 'REP', result['firstName']+ ' '+ result['lastName'], result['totalVotes']])
+        results.append([county, result['precinctName'], office, None, result['party'], result['firstName']+ ' '+ result['lastName'], result['totalVotes']])
 
 
-with open('20200602__pa__primary__monroe__precinct.csv', 'wt') as csvfile:
+with open('20201103__pa__general__monroe__precinct.csv', 'wt') as csvfile:
     w = csv.writer(csvfile)
     headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes']
     w.writerow(headers)
