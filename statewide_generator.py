13213effe3d4eec9467a1d0878c4fd48b036dec6
Derek Willis <dwillis@gmail.com>
2022-08-21 21:17:36 -0400

    added Auditor General

--- statewide_generator.py
+++ statewide_generator.py
@@ -2,8 +2,8 @@ import os
 import glob
 import csv
 
-year = '2018'
-election = '20181106'
+year = '2020'
+election = '20201103'
 path = election+'*precinct.csv'
 output_file = election+'__pa__general__precinct.csv'
 
@@ -24,6 +24,7 @@ def generate_headers(year, path):
 
 def generate_offices(year, path):
     os.chdir(year)
+    os.chdir('counties')
     offices = []
     for fname in glob.glob(path):
         with open(fname, "r") as csvfile:
@@ -45,14 +46,35 @@ def generate_consolidated_file(year, path, output_file):
             print(fname)
             reader = csv.DictReader(csvfile)
             for row in reader:
-                if row['office'].strip() in ['Straight Party', 'President', 'Governor', 'Secretary of State', 'Railroad Commissioner', 'State Auditor', 'State Treasurer', 'Commissioner of Agriculture & Commerce', 'Commissioner of Insurance', 'Attorney General', 'U.S. House', 'State Senate', 'State House', 'U.S. Senate', 'House of Delegates', 'State Representative', 'Registered Voters', 'Ballots Cast']:
-                    if all(k in set(row) for k in ['absentee', 'election_day']):
-                        results.append([row['county'], row['precinct'], row['office'], row['district'], row['candidate'], row['party'], row['votes'], row['absentee'], row['election_day']])
+                if row['office'].strip() in ['Straight Party', 'President', 'Governor', 'Secretary of State', 'Railroad Commissioner', 'State Auditor', 'Auditor General', 'State Treasurer', 'Commissioner of Agriculture & Commerce', 'Commissioner of Insurance', 'Attorney General', 'U.S. House', 'State Senate', 'State House', 'U.S. Senate', 'House of Delegates', 'State Representative', 'Registered Voters', 'Ballots Cast', 'Ballots Cast Blank']:
+                    if 'absentee' in row:
+                        absentee = row['absentee']
                     else:
-                        results.append([row['county'], row['precinct'], row['office'], row['district'], row['candidate'], row['party'], row['votes'], None, None])
+                        absentee = None
+                    if 'mail' in row:
+                        mail = row['mail']
+                    else:
+                        mail = None
+                    if 'election_day' in row:
+                        election_day = row['election_day']
+                    else:
+                        election_day = None
+                    if 'provisional' in row:
+                        provisional = row['provisional']
+                    else:
+                        provisional = None
+                    if 'military' in row:
+                        military = row['military']
+                    else:
+                        military = None
+                    if 'extra' in row:
+                        extra = row['extra']
+                    else:
+                        extra = None
+                    results.append([row['county'], row['precinct'], row['office'], row['district'], row['candidate'], row['party'], row['votes'], election_day, absentee, mail, provisional, military, extra])
     os.chdir('..')
     os.chdir('..')
     with open(output_file, "w") as csv_outfile:
         outfile = csv.writer(csv_outfile)
-        outfile.writerow(['county','precinct', 'office', 'district', 'candidate', 'party', 'votes', 'absentee', 'election_day'])
+        outfile.writerow(['county','precinct', 'office', 'district', 'candidate', 'party', 'votes', 'election_day', 'absentee', 'mail', 'provisional', 'military', 'extra'])
         outfile.writerows(results)
